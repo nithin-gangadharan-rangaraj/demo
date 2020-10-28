bd1704cf359473fd27ed9136b7cc503cfef7d849
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2020-10-28 09:58:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
+from transformers import Pipeline
+from transformers import pipeline
 import streamlit as st
 
-st.title('Hello GR')
+@st.cache
+def get_qa_pipeline() -> Pipeline:
+    qa = pipeline("question-answering")
+    return qa
+
+def answer_question(pipeline: Pipeline, question: str, paragraph: str) -> dict:
+    input = {
+        "question": question,
+        "context": paragraph
+    }
+    return pipeline(input)
+
+def format_text(paragraph: str, start_idx: int, end_idx: int) -> str:
+    return paragraph[:start_idx] + "**" + paragraph[start_idx:end_idx] + "**" + paragraph[end_idx:]
+
+if __name__ == "__main__":
+    passage = st.text_input("PASSAGE", "")
+    question = st.text_input("QUESTION", "")
+
+    if question != "":
+      pipeline = get_qa_pipeline()
+      #st.write(pipeline.model)
+      #st.write(pipeline.model.config)
+      try:
+        answer = answer_question(pipeline, question, passage)
+        #start_idx = answer["start"]
+        #end_idx = answer["end"]
+        st.write("Prediction:", answer["answer"])
+      except:
+        st.write("You must provide a valid passage")
